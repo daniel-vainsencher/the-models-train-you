c058fdbb329654f7797588839bca508c85c8de01
Daniel Vainsencher <daniel.vainsencher@gmail.com>
2023-05-07 15:01:41 -0400

    Add two very initial scripts
    - llm_vicuna.py: send a question to vicuna13B hosted on Modal, get an answer.
    - deepspeed_modal_sft.py: run a DS training demo on a single A100 on modal.
    
    These document sufficient conditions for things to not crash.
    Not intended to be either usable or well written yet.

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # the-models-train-you
+
+A few scripts for training/using LLM models via Modal. 
+
+Maturity level: worked for me once, probably out of date.
+
+
---
+++ src/deepspeed_modal_sft.py
@@ -0,0 +1,39 @@
+import modal
+import subprocess
+
+dschat_image = (
+    modal.Image.from_dockerhub(
+        "nvidia/cuda:11.7.0-devel-ubuntu20.04",
+        setup_dockerfile_commands=[
+            "RUN apt-get update",
+            "RUN apt-get install -y python3 python3-pip python-is-python3",
+        ],
+    )
+    .apt_install("git", "gcc", "build-essential")
+    # Create and enter venv in image prep to messing with the gloal env which pip complains about.
+    .pip_install("deepspeed[fused_adam]>=0.9.2")
+    .run_commands(
+        "git clone https://github.com/microsoft/DeepSpeedExamples.git",
+        "cd DeepSpeedExamples/applications/DeepSpeed-Chat/ && pip install -r requirements.txt",
+    )
+)
+
+stub = modal.Stub("gpu_check", image=dschat_image)
+
+@stub.function(gpu="A100")
+def tell_me_stuff():
+    cmds = ["nvidia-smi -L",
+            "uname -a",
+            "pip list",
+            "cd /DeepSpeedExamples/applications/DeepSpeed-Chat/training/step1_supervised_finetuning/ && /usr/bin/python -u -m deepspeed.launcher.launch --world_info=eyJsb2NhbGhvc3QiOiBbMF19 --master_addr=127.0.0.1 --master_port=29500 --enable_each_rank_log=None main.py --model_name_or_path facebook/opt-1.3b --gradient_accumulation_steps 8 --lora_dim 128 --gradient_checkpoint --zero_stage 0 --deepspeed --output_dir /DeepSpeedExamples/applications/DeepSpeed-Chat/output/actor-models/1.3b"
+            ]
+    return [subprocess.check_output(cmd, shell=True).decode('utf-8').strip() for cmd in cmds]
+
+# "python train.py --actor-model facebook/opt-1.3b --reward-model facebook/opt-350m --deployment-type single_node"
+
+@stub.local_entrypoint()
+def main():
+    print("Run:\n", tell_me_stuff.call())
+
+
+
